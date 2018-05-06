ff48ffe1fbe3935e6afb2cb0149bc61d8a42b840
alexandrubujor <alexandru.bujor@bluedrive.ro>
2018-05-07 00:24:08 +0200

    Allow Any in views

--- blueocr/ocr/views.py
+++ blueocr/ocr/views.py
@@ -1,27 +1,32 @@
 from rest_framework import generics, permissions
 from .models import OcrDocument, OcrResult
 from .serializers import OcrDocumentSerializer, OcrResultSerializer
+from rest_framework.permissions import AllowAny
 
 
 class OcrDocumentList(generics.ListCreateAPIView):
     model = OcrDocument
     serializer_class = OcrDocumentSerializer
     queryset = OcrDocument.objects.all()
+    permission_classes = (AllowAny,)
 
 
 class OcrDocumentDetail(generics.RetrieveDestroyAPIView):
     model = OcrDocument
     serializer_class = OcrDocumentSerializer
     queryset = OcrDocument.objects.all()
+    permission_classes = (AllowAny,)
 
 
 class OcrResultList(generics.ListAPIView):
     model = OcrResult
     serializer_class = OcrResultSerializer
     queryset = OcrResult.objects.all()
+    permission_classes = (AllowAny,)
 
 
 class OcrResultDetail(generics.RetrieveDestroyAPIView):
     model = OcrResult
     serializer_class = OcrResultSerializer
     queryset = OcrResult.objects.all()
+    permission_classes = (AllowAny,)
